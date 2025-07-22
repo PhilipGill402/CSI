5e6930d8ce1d363bb0ca727470dcf5ec58201349
FurryBasket9978 <pgillbama@icloud.com>
2025-07-21 19:01:03 -0500

    added a check to make sure a file path was provided

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,17 @@
 #include <fstream>
 
 int main(int argc, char* argv[]){
+    if (argc < 2){
+        std::cerr << "Error: expected a file\n";
+        return -1;
+    } 
+    
     std::string file_path = argv[1]; 
     //load text from file
     std::ifstream file(file_path);
     if (!file){
         std::cerr << "Error: could not open file " << file_path << "\n";
-        return 1;
+        return -1;
     } 
     std::string tmp;
     std::string text;
@@ -29,6 +34,6 @@ int main(int argc, char* argv[]){
     AST* tree = parser.parse();
     SemanticAnalyzer semantics = SemanticAnalyzer();
     semantics.visit(tree);
-    Interpreter interpreter = Interpreter(tree); 
+    Interpreter interpreter = Interpreter(tree);
     interpreter.interpret();
 }
